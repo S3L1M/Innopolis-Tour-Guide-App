c5bb6bc99e9fa4c6e6d15567a494ea230fba0380
Mohamed Selim <dev.m.selim@gmail.com>
2020-06-11 03:17:24 +0200

    rename image names to lowercase

--- app/src/main/java/com/example/tourguideapp/TopVisitedFragment.java
+++ app/src/main/java/com/example/tourguideapp/TopVisitedFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -19,7 +21,8 @@ public class TopVisitedFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.list, container, false);
         super.onCreate(savedInstanceState);
 
-
+        ArrayList<Item> items = new ArrayList<Item>();
+        items.add(new Item(R.drawable.innopolis_university, R.string.innopolis_university));
 
         return rootView;
     }
